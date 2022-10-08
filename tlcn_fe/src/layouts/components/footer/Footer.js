0cec3d92ad27c24401b50950f2998f20c65ca911
Nguyen Hoang Chi Vi <19110021@student.hcmute.edu.vn>
2022-10-08 15:02:06 +0700

    footer

--- tlcn_fe/src/layouts/components/footer/Footer.js
+++ tlcn_fe/src/layouts/components/footer/Footer.js
@@ -1,5 +1,15 @@
+import classNames from 'classnames/bind';
+import styles from './Footer.module.scss';
+
+const cx = classNames.bind(styles);
 function Footer() {
-    return <footer>Footer</footer>;
+    return (
+        <footer className={cx('footer')}>
+            <span className={cx('footer__context')}>
+                Copyright © 2022 TLCN CLC Tiếng Việt
+            </span>
+        </footer>
+    );
 }
 
 export default Footer;
